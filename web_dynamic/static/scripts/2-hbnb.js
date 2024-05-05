b7e53a9318ecf4652ba3e8c3524c6cd12fcf7e0c
Richouf95 <wourichouf95@gmail.com>
2024-05-05 02:14:24 +0100

    3. API status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,23 @@
+$(() => {
+
+  $.get('http://0.0.0.0:5001/api/v1/status').done((data, status) => {
+    if (data.status === 'OK') $('#api_status').addClass('available');
+    else $('#api_status').removeClass('available');
+  })
+
+  const amenity_selected = {};
+  $('input[type=checkbox]').change(function () {
+    if ($(this).prop('checked')) {
+      amenity_selected[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else if (!$(this).prop('checked')) {
+      delete amenity_selected[$(this).attr('data-id')];
+    }
+    if (Object.keys(amenity_selected).length === 0) {
+      $('div.amenities h4').html('&nbsp');
+    } else {
+      checked_amenity = Object.values(amenity_selected).join(', ');
+      if (checked_amenity.length > 60) $('div.amenities h4').text(checked_amenity.substring(0, 60) + '...')
+      else $('div.amenities h4').text(checked_amenity);
+    }
+  });
+});
